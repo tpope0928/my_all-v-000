d7aa1e425b382f745bfffa650eb9381b7384fe22
Thomas Pope <tpope0928@gmail.com>
2018-08-20 14:36:15 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,11 +1,2 @@
 require 'pry'
 
-def my_all?(collection)
-  i = 0
-  block_return_values = []
-  while i < collection.length
-    block_return_values <<
-yeild(collection[i])
-  i = i + 1
-end
-end
